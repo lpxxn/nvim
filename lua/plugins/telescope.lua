3e6332ca8810d54ca065d6843c0e9167fe28c6ba
lpxxn <mi_duo@live.com>
2023-07-03 11:37:16 +0800

    feat: quick start

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,3 +3,8 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fw', function()
+  require("telescope.builtin").live_grep {
+        additional_args = function(args) return vim.list_extend(args, { "--hidden", "--no-ignore" }) end,
+      }
+end, {})
